6bbc29cbc8314972a149f044094aa651b7061124
André Victor Leal <100169443+h1xx@users.noreply.github.com>
2023-10-25 21:44:09 -0300

    Add files via upload

---
+++ POO2Sem/aula20231018/src/aula20231025/aula/poo/TaskRepository.java
@@ -0,0 +1,33 @@
+package aula20231025.aula.poo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+	
+	private static List<Task> records = new ArrayList<Task>();
+	private static Integer nextCode = 1;
+	
+	public Task findByCode(final Integer code) {
+		return records.stream().filter((current)-> 
+		current.getCode().equals(code)).findFirst().orElse(null);
+	}
+	public List<Task>findAll(){
+		return records;
+	}
+	
+	public void insert (Task task) {
+		task.setCode(nextCode++);
+		records.add(task);
+	}
+	public void update (Task task) {
+		records = records.stream().map((current)-> {
+			return current.getCode().equals(task.getCode()) ? task: current;
+		}).toList();
+	}
+	
+	public void delete (Integer code) {
+		records = records.stream().filter((current)->
+			!current.getCode().equals(code)).toList();
+	}
+}
